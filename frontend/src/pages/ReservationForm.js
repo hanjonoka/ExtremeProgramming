7c9fa575cc71cbee31b51053062209db426a4297
hanjonoka <joris.gallene@ensta-paris.fr>
2023-02-07 19:28:49 +0100

    début formulaire resa

--- frontend/src/pages/ReservationForm.js
+++ frontend/src/pages/ReservationForm.js
@@ -1,5 +1,5 @@
 import {Link, useSearchParams} from "react-router-dom";
-import {Button, Card, CardContent, FormControl, Typography} from "@mui/material";
+import {Button, Card, CardContent, FormControl, TextField, Typography} from "@mui/material";
 import {useEffect, useState} from "react";
 import SeanceCard from "../components/SeanceList/SeanceCard";
 import {getSeanceById} from "../api/DataApi";
@@ -10,8 +10,22 @@ export default function ReservationForm() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [seanceId, setSeanceId] = useState(0);
     const [seance, setSeance] = useState();
+    const [nb_places, setnb_places] = useState(0);
+    const changeNbPlaces = (event) => {
+        setnb_places(event.target.value);
+    }
+    const [clientid, setClientid] = useState(0);
+    const changeClientid = (event) => {
+        setClientid(event.target.value);
+    }
+
+    const submitReservation = (event) => {
+        let reservation = {seance_id: seanceId, nb_places: nb_places, client_id: clientid};
+
+    }
 
     useEffect(() => {
+        setSeanceId(parseInt(searchParams.get("id")));
         getSeanceById(parseInt(searchParams.get("id"))).then((response)=>{
             setSeance(response.data);
         })
@@ -20,7 +34,10 @@ export default function ReservationForm() {
         <div>
             {seance!=null ? <SeanceCard reservation={true} seance={seance} /> : "" }
             <FormControl fullWidth>
-
+                <TextField id="id-field" type="hidden" value={seanceId}></TextField>
+                <TextField id="nb_places-field" label="nb_places" type="number" value={nb_places} onChange={changeNbPlaces}></TextField>
+                <TextField id="cliend-field" label="id_client" type="number"  value={clientid} onChange={changeClientid}></TextField>
+                <Button onClick={submitReservation}>Réserver</Button>
             </FormControl>
         </div>
     )
